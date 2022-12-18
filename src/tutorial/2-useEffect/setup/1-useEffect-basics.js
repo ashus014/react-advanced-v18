86fa0964eefccfb93130b733cfa38bc575ddf5af
Ashu Singh <ashu.ask.singh@gmail.com>
2022-12-18 10:50:23 +0530

    useEffect basics

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log("Ashu Singh");
+    if (value > 0) {
+      document.title = `New Messages(${value}) `;
+    }
+    // if only '[]' is specified, it'll only run on the initial render
+  }, [value]);
+  console.log("Ashu Singh : 2");
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        Click Me
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
